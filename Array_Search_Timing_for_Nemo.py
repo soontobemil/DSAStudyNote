0646b3f6d79daf87f94373aee985a5ef60c3f0fe
Jay Sim <jay.sim@vshn.net>
2024-09-04 12:34:25 -0700

    Add comments in lines of code

--- Array_Search_Timing_for_Nemo.py
+++ Array_Search_Timing_for_Nemo.py
@@ -2,7 +2,7 @@ import time
 
 nemo = ['nemo']
 everyone = ['Alice', 'Bob', 'Charlie', 'Diana', 'Eve', 'Frank', 'Grace', 'nemo', 'Hannah', 'Ivy', 'Jack']
-large = ["nemo"] * 1000000
+large = ["nemo"] * 1000000 # Creates 100 arrays
 
 def find_nemo (array):
     t0 = time.perf_counter()
@@ -10,6 +10,6 @@ def find_nemo (array):
         if array[i] == "nemo":
             print("Found nemo!!")
     t1 = time.perf_counter()
-    print(f"Time taken {t1 - t0} milliseconds")
+    print(f"Time taken {t1 - t0} milliseconds") # This measures the time it has taken to run the function
 
 find_nemo(large)
